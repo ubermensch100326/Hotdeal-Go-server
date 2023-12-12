22daa8e4e94ca8478ffbf448457d31cfbc8dcf0f
이희웅 <dlgmldnd017@naver.com>
2023-12-12 17:01:41 +0900

    (추가) 언더스코어 세팅, 좋아요 탑3,  (수정) 중복된 메서드 삭제

--- src/main/java/com/budge/hotdeal_go/controller/SearchController.java
+++ src/main/java/com/budge/hotdeal_go/controller/SearchController.java
@@ -1,17 +1,9 @@
 package com.budge.hotdeal_go.controller;
 
-import java.io.IOException;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,9 +18,11 @@ import com.budge.hotdeal_go.model.service.SearchService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/hotdeal")
+@RequiredArgsConstructor
 @Api(tags = "HotDealSearch", description = "Search Controller")
 public class SearchController {
 
@@ -40,43 +34,92 @@ public class SearchController {
         this.searchService = searchService;
     }
 
-    @ApiOperation(value = "에펨코리아 핫딜 정보 반환", notes = "해당 커뮤니티에서 핫딜 정보를 리스트로 반환한다.", response = List.class)
-    @GetMapping("/fmkorea")
+    @ApiOperation(value = "핫딜 크롤링 정보 반환", notes = "해당 커뮤니티에서 핫딜 정보를 리스트로 반환한다.", response = List.class)
+    @GetMapping("/info")
     public ResponseEntity<List<HotDealDto>> doSearchFmkorea(
+            @RequestParam(required = false, defaultValue = ",") @ApiParam(value = "검색할 사이트 이름(default=0, 에펨코리아=1, 퀘이사존=2, 루리웹=3)") String siteno,
             @RequestParam(required = false, defaultValue = "1") @ApiParam(value = "현재 페이지 번호(defualt=1)") int pgno,
             @RequestParam(required = false, defaultValue = "50") @ApiParam(value = "페이지당 글의 수(default=50)") int npp)
             throws SQLException {
-        List<HotDealDto> list = searchService.getFmkorea(pgno, npp);
-        return ResponseEntity.ok(list);
-    }
+        List<HotDealDto> list = new ArrayList<>();
 
-    @ApiOperation(value = "퀘이사존 핫딜 정보 반환", notes = "해당 커뮤니티에서 핫딜 정보를 리스트로 반환한다.", response = List.class)
-    @GetMapping("/quasarzone")
-    public ResponseEntity<List<HotDealDto>> doSearchQuasarZone(
-            @RequestParam(required = false, defaultValue = "1") @ApiParam(value = "현재 페이지 번호(default=1)") int pgno,
-            @RequestParam(required = false, defaultValue = "50") @ApiParam(value = "페이지당 글의 수(default=50)") int npp)
-            throws SQLException {
-        List<HotDealDto> list = searchService.getQuasarZone(pgno, npp);
-        return ResponseEntity.ok(list);
-    }
+        if (siteno.contains("0")) {
+            return ResponseEntity.ok(searchService.getListAll(pgno, npp));
+        }
 
-    @ApiOperation(value = "루리웹 핫딜 정보 반환", notes = "해당 커뮤니티에서 핫딜 정보를 리스트로 반환한다.", response = List.class)
-    @GetMapping("/ruliweb")
-    public ResponseEntity<List<HotDealDto>> doSearchRuliweb(
-            @RequestParam(required = false, defaultValue = "1") @ApiParam(value = "현재 페이지 번호(default=1)") int pgno,
-            @RequestParam(required = false, defaultValue = "50") @ApiParam(value = "페이지당 글의 수(default=50)") int npp)
-            throws SQLException {
-        List<HotDealDto> list = searchService.getRuliweb(pgno, npp);
+        String[] arr = siteno.split(",");
+        for (String no : arr) {
+            List<HotDealDto> tmp = new ArrayList<>();
+            switch (no) {
+                case "1":
+                    tmp = searchService.getHotDealInfoBySiteName("fmkorea", pgno, npp);
+                    break;
+
+                case "2":
+                    tmp = searchService.getHotDealInfoBySiteName("quasarzone", pgno, npp);
+                    break;
+
+                case "3":
+                    tmp = searchService.getHotDealInfoBySiteName("ruliweb", pgno, npp);
+                    break;
+            }
+            list.addAll(tmp);
+        }
         return ResponseEntity.ok(list);
     }
 
     @ApiOperation(value = "핫딜 정보 검색", notes = "게시글 제목 기준으로 데이터를 반환합니다.", response = List.class)
     @GetMapping("/{title}")
     public ResponseEntity<List<HotDealDto>> doSearchForTheHotDealInfo(
-            @PathVariable("title") @ApiParam(value = "얻어올 글의 제목", required = true) String title)
+            @ApiParam(value = "얻어올 글의 제목", required = true) String title,
+            @RequestParam(required = false, defaultValue = ",") @ApiParam(value = "검색할 사이트 이름(default=0, 에펨코리아=1, 퀘이사존=2, 루리웹=3)") String siteno,
+            @RequestParam(required = false, defaultValue = "1") @ApiParam(value = "현재 페이지 번호(defualt=1)") int pgno,
+            @RequestParam(required = false, defaultValue = "50") @ApiParam(value = "페이지당 글의 수(default=50)") int npp)
             throws SQLException {
 
-        List<HotDealDto> list = searchService.getList(title);
+        List<HotDealDto> list = new ArrayList<>();
+
+        if (siteno.contains("0")) {
+            return ResponseEntity.ok(searchService.getList(title));
+        }
+
+        String[] arr = siteno.split(",");
+        for (String no : arr) {
+            List<HotDealDto> tmp = new ArrayList<>();
+            switch (no) {
+                case "1":
+                    tmp = searchService.getListBySiteName(title, "fmkorea", pgno, npp);
+                    break;
+
+                case "2":
+                    tmp = searchService.getListBySiteName(title, "quasarzone", pgno, npp);
+                    break;
+
+                case "3":
+                    tmp = searchService.getListBySiteName(title, "ruliweb", pgno, npp);
+                    break;
+            }
+            list.addAll(tmp);
+        }
+
+        return ResponseEntity.ok(list);
+    }
+
+    // @ApiOperation(value = "조회수 Top3 게시글 반환", notes = "전체 커뮤니티에서 조회수가 높은 3개의 게시글을
+    // 반환한다.", response = List.class)
+    // @GetMapping("/top3")
+    // public ResponseEntity<List<HotDealDto>> doGetTop3View() {
+    // List<HotDealDto> list = new ArrayList<>();
+    // list = searchService.getTop3View();
+
+    // return ResponseEntity.ok(list);
+    // }
+
+    @ApiOperation(value = "좋아요 Top3 게시글 반환", notes = "전체 커뮤니티에서 좋아요가 높은 3개의 게시글을 반환한다.", response = List.class)
+    @GetMapping("/like/top3")
+    public ResponseEntity<List<HotDealDto>> doGetTop3Like() throws SQLException {
+        List<HotDealDto> list = searchService.getTop3Like();
+
         return ResponseEntity.ok(list);
     }
 }
--- src/main/java/com/budge/hotdeal_go/model/mapper/SearchMapper.java
+++ src/main/java/com/budge/hotdeal_go/model/mapper/SearchMapper.java
@@ -10,13 +10,26 @@ import com.budge.hotdeal_go.model.dto.HotDealDto;
 
 @Mapper
 public interface SearchMapper {
+    // 크롤링 정보를 DB에 등록하는 함수
     boolean regist(List<HotDealDto> dto) throws SQLException;
 
-    List<HotDealDto> getFmkorea(@Param("pgno") int pgno, @Param("npp") int npp) throws SQLException;
+    // 전체 커뮤니티에서 핫딜 정보를 가져오는 함수
+    List<HotDealDto> getListAll(@Param("pgno") int pgno, @Param("npp") int npp) throws SQLException;
 
-    List<HotDealDto> getQuasarZone(@Param("pgno") int pgno, @Param("npp") int npp) throws SQLException;
+    // 해당 커뮤니티에서 핫딜 정보를 가져오는 함수
+    List<HotDealDto> getHotDealInfoBySiteName(String siteName, @Param("pgno") int pgno, @Param("npp") int npp)
+            throws SQLException;
 
-    List<HotDealDto> getRuliweb(@Param("pgno") int pgno, @Param("npp") int npp) throws SQLException;
-
+    // 전체 커뮤니티에서 제목에 해당하는 게시글을 가져오는 함수
     List<HotDealDto> getList(String title) throws SQLException;
+
+    // 해당 커뮤니티에서 제목에 해당하는 게시글을 가져오는 함수
+    List<HotDealDto> getListBySiteName(String title, String siteName, @Param("pgno") int pgno, @Param("npp") int npp)
+            throws SQLException;
+
+    // 조회수 탑3 게시글 가져오기
+    List<HotDealDto> getTop3View() throws SQLException;
+
+    // 좋아요 탑3 게시글 가져오기
+    List<HotDealDto> getTop3Like() throws SQLException;
 }
